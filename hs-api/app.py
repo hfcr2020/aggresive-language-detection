4fd973b1bf9021225e318eb0f9c4659f27938e27
hfcr2017 <H3c70r.95261>
2020-08-23 20:01:49 +0200

    Move API to separate folder and leave notebook alone with dataset

--- hs-api/app.py
+++ hs-api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import tensorflow as tf
 import re
 import string
@@ -26,17 +26,21 @@ def custom_standardization(input_data):
     return tf.strings.regex_replace(remove_amp, 'http.+$', '')
 
 
-model = keras.models.load_model('../hate_speech_model',
+model = keras.models.load_model('hate_speech_model',
                                 custom_objects={'TextVectorization': TextVectorization,
-                                                'custom_standardization': custom_standardization}
-                                )
+                                                'custom_standardization': custom_standardization})
 
-@app.route("/", methods=["POST"])
-def index():
+@app.route('/')
+def root():
+    print("Render index")
+    return render_template('index.html')
+
+@app.route("/validate", methods=["POST"])
+def validate():
     print("Request received")
     tweet = request.json
     print("Tweet: " + tweet['tweet'])
     prediction = model.predict([str(tweet['tweet'])])
-    hate_speech = prediction[0][0] > 0.5
-    print("Response:  " + str(hate_speech))
-    return str(hate_speech)
+    is_hate_speech = not(prediction[0][0] > 0.5)
+    print("Response:  " + str(is_hate_speech))
+    return str(is_hate_speech).lower()
